a6abd8a985cd2d3d856b2003d5a77b273f145d37
Syifa Khairina <syifa.khairina.tif421@polban.ac.id>
2022-12-01 08:28:49 +0700

    update

---
+++ application/views/Pages/Form_Product.php
@@ -0,0 +1,65 @@
+
+      <!-- partial -->
+      <div class="main-panel">        
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-md-6 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <h4 class="card-title">Product Form</h4>
+                  <p class="card-description">
+                    Please insert the data of the product 
+                  </p>
+                  <form class="forms-sample">
+                    <div class="form-group">
+                      <label for="exampleInputUsername1">Product ID</label>
+                      <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Product ID">
+                    </div>
+                    <div class="form-group">
+                      <label for="exampleInputEmail1">Product Name</label>
+                      <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Product name">
+                    </div>
+                    <div class="form-group">
+                    <label for="exampleSelectGender">Category</label>
+                        <select class="form-control" id="exampleSelectGender">
+                          <option>Pet Food</option>
+                          <option>Pet Health Supplies</option>
+                          <option>Grooming Supplies</option>
+                          <option>Toys</option>
+                          <option>Other</option>
+                        </select>
+                  </div>
+                  <div class="form-group">
+                      <label for="exampleInputUsername1">Quantity</label>
+                      <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Quantity (Pcs)">
+                    </div>
+                  <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Entry Date</label>
+                          <div class="col-sm-9">
+                            <input class="form-control" placeholder="YYYY-MM-DD"/>
+                          </div>
+                          </div>      
+                    <div class="form-group">
+                    <label for="exampleInputUsername1">Price</label>
+                      <input type="text" class="form-control" id="exampleInputUsername1" placeholder="Price">
+                    </div>
+                    <div class="form-group row">
+                          <label class="col-sm-3 col-form-label">Expired Date</label>
+                          <div class="col-sm-9">
+                            <input class="form-control" placeholder="YYYY-MM-DD"/>
+                          </div>
+                          </div>
+                    <div class="form-check form-check-flat form-check-primary">
+                      <label class="form-check-label">
+                        <input type="checkbox" class="form-check-input">
+                        are you sure want to submit this form?
+                      </label>
+                    </div>
+                    <button type="submit" class="btn btn-primary mr-2">Submit</button>
+                    <button class="btn btn-light">Cancel</button>
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
